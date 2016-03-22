2594df62c07087836d99578490c2e0920eb58850
ngasull <blint6@gmail.com>
2016-03-23 00:25:08 +0100

    Headers are now multiple. Added a Store with mimes

--- SimpleServe.hs
+++ SimpleServe.hs
@@ -10,32 +10,39 @@ import Network
 import System.IO
 import Text.Read (readMaybe)
 
+type Ext = String
+type Mime = String
+
 data Method = DELETE | GET | HEAD | POST | PUT deriving (Read, Show)
 
 data Request = Request { method :: Method
                        , uri :: String
-                       , headers :: M.Map String String
+                       , headers :: M.Map String [String]
                        } deriving (Show)
 
 data Response = Response { body :: String
                          , code :: Int
                          }
 
+data Store = Store { mimes :: M.Map Mime [Ext]
+                   } deriving (Show)
+
 instance Show Response where
   show Response { body, code } = concat [ show code, " : ", body, "\n" ]
 
-main = withSocketsDo $
-  bracket (listenOn (PortNumber 8080)) sClose loop
+main :: IO ()
+main = do
+  store <- makeStore
+  withSocketsDo $ bracket (listenOn (PortNumber 8080)) sClose (loop store)
 
-loop :: Socket -> IO ()
-loop sock = do
+loop :: Store -> Socket -> IO ()
+loop store sock = do
   (h,_,_) <- accept sock
-  forkFinally (handleRequest h) (const $ hClose h)
-  loop sock
+  forkFinally (handleRequest store h) (const $ hClose h)
+  loop store sock
 
-handleRequest :: Handle -> IO ()
-handleRequest h = do
-  -- body <- withFile "Setup.hs" ReadMode hGetContents
+handleRequest :: Store -> Handle -> IO ()
+handleRequest store h = do
   req <- parseRequest <$> getHeaders h
   let res = case req of
           Just Request { method, uri } -> Response
@@ -46,14 +53,19 @@ handleRequest h = do
   hPutStr h $ httpRequest res
 
 getHeaders :: Handle -> IO [String]
-getHeaders h = reverse <$> getHeaders' [] h
+getHeaders h = reverse <$> getHeaders' []
+  where getHeaders' :: [String] -> IO [String]
+        getHeaders' acc = do
+          line <- hGetLine h
+          if line == "\r"
+            then return acc
+            else getHeaders' (line:acc)
 
-getHeaders' :: [String] -> Handle -> IO [String]
-getHeaders' acc h = do
-  line <- hGetLine h
-  if line == "\r"
-    then return acc
-    else getHeaders' (line:acc) h
+getMimeDictionnary :: IO (M.Map Mime [Ext])
+getMimeDictionnary = M.fromList . mapMimeToExt <$> readFile "/etc/mime.types"
+  where mapMimeToExt contents = [(mime, exts) | l <- lines contents , not (null l) , head l /= '#'
+                                              , let wds = words l , length wds > 1
+                                              , let mime:exts = wds ]
 
 httpRequest :: Response -> String
 httpRequest res = concat
@@ -64,14 +76,19 @@ httpRequest res = concat
   ]
   where content = show res
 
+makeStore :: IO Store
+makeStore = do
+  mimes <- getMimeDictionnary
+  return Store { mimes }
+
 parseRequest :: [String] -> Maybe Request
 parseRequest reqHeaders =
-  if (not.null) reqHeaders && length hh == 3 && isJust mMaybe
+  if not (null reqHeaders) && length hh == 3 && isJust mMaybe
   then Just Request { method, headers, uri }
   else Nothing
-  where hh = words . head $ reqHeaders
+  where hh = words (head reqHeaders)
         [m, uri, _] = hh
         mMaybe = readMaybe m :: Maybe Method
         Just method = mMaybe
-        headers = M.fromList [ (h, v) | l <- map (splitOn ": ") (tail reqHeaders)
-                                      , length l >= 2 , let h:v:_ = l ]
+        headers = M.fromListWith (++) [(h, [v]) | l <- map (splitOn ": ") (tail reqHeaders)
+                                                , length l == 2 , let h:v:_ = l ]
